7b610f6d504b84d512302c2292a2bd516db88cb6
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-15 22:57:01 +0200

    Initial commit

---
+++ my-server/server.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user: 'dxol58wulq0axl3f',
+    password: 'ecguod0gceobjoii',
+    database: 'jniqw847exo0xq4a'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+app.get('/api/groupsList', (req, res) => {
+    const sql = 'SELECT group_code FROM groups_TB';
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error fetching data');
+            return;
+        }
+        res.json(result);
+    });
+});
+
+app.get('/api/teachersList', (req, res) => {
+    const sql = 'SELECT full_name FROM teachers_TB';
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error fetching data');
+            return;
+        }
+        res.json(result);
+    });
+});
+
+
+app.get('/api/getGroup', (req, res) => {
+    const groupName = `"${req.query.groupName}"`;
+    const semester = req.query.semester;
+    const sql = `
+    SELECT 
+        s.week_number, 
+        s.day_number, 
+        s.pair_number, 
+        c.subject_name,
+        s.lesson_type, 
+        s.visit_format, 
+        a.building,
+        a.audience_number,
+        (
+            SELECT JSON_ARRAYAGG(
+                JSON_OBJECT(t.full_name, t.post)
+            )
+            FROM teachers_TB t
+            WHERE JSON_CONTAINS(s.teachers_list, CONCAT('"', t.full_name, '"'), '$')
+        ) AS teachers_with_post
+    FROM 
+        schedule_TB s
+    JOIN 
+        curriculum_TB c ON s.subject_id = c.id
+    LEFT JOIN
+        audience_TB a ON s.audience = a.id  
+    WHERE 
+        JSON_CONTAINS(s.groups_list, ?, "$") 
+        AND s.semester_number = ?;
+  `;
+    db.query(sql, [groupName, semester], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error fetching data');
+            return;
+        }
+        res.json(result);
+    });
+});
+
+app.get('/api/getTeacher', (req, res) => {
+    const teacherName = `"${req.query.teacherName}"`;
+    const semester = req.query.semester;
+    const sql = `
+    SELECT 
+        s.week_number, 
+        s.day_number, 
+        s.pair_number, 
+        c.subject_name,
+        s.groups_list, 
+        s.lesson_type, 
+        s.visit_format, 
+        a.building,
+        a.audience_number
+    FROM 
+        schedule_TB s
+    JOIN 
+        curriculum_TB c ON s.subject_id = c.id
+    LEFT JOIN
+        audience_TB a ON s.audience = a.id  
+    WHERE 
+        JSON_CONTAINS(s.teachers_list, ?, "$") 
+        AND s.semester_number = ?;
+  `;
+    db.query(sql, [teacherName, semester], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error fetching data');
+            return;
+        }
+        res.json(result);
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
