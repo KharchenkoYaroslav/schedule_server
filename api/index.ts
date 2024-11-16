1b3027cf5ac9ad2156e0d23c1f24608d68ddd296
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-16 09:30:30 +0200

    Changes to be committed:

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
