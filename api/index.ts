d40a0805aadaf5aa0980b10b3a2611ffc7d372ed
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-16 11:06:52 +0200

    Changes to be committed: 	modified:   api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,34 +3,18 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-
+app.use(express.json());
 // Налаштування CORS
 const corsOptions = {
-    origin: 'https://schedule-eosin-two.vercel.app', // Додайте ваш домен
-    optionsSuccessStatus: 200 // Деякі старі браузери (IE11, різні версії Safari) мають проблеми з 204
+    origin: 'https://schedule-eosin-two.vercel.app', 
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
 
-app.use(function (req, res, next) {
+app.options('/api/getGroup', cors(corsOptions));
+app.options('/api/getTeacher', cors(corsOptions));
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://schedule-eosin-two.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
