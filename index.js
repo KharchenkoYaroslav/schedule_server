37d767615d77c71c193fe9edde6f0650c140de5b
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-30 22:57:44 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ import mysql from 'mysql2/promise';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import crypto from 'crypto';
-import cors from 'cors';
-import helmet from 'helmet';
-import compression from 'compression';
-import morgan from 'morgan';
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(helmet());
-app.use(compression());
-app.use(morgan('combined'));
 
 
 const pool = mysql.createPool({
