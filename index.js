eeff7c6180e31058a4e37e955135d500d561d6c1
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-30 23:10:59 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import crypto from 'crypto';
 import cors from 'cors';
-import helmet from 'helmet';
 import compression from 'compression';
 
 
@@ -13,7 +12,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
 app.use(compression());
 
 
